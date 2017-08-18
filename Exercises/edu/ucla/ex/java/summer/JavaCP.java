136a7a851b12667f9d446df6088dc0917591e6c4
Juan Carlos <jcrogel@gmail.com>
2017-08-17 20:34:33 -0700

    Adds JavaCP

---
+++ Exercises/edu/ucla/ex/java/summer/JavaCP.java
@@ -0,0 +1,20 @@
+package edu.ucla.ex.java.summer;
+
+import java.util.Scanner;
+
+public class JavaCP {
+    public static void main(String args[]){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Source:");
+        String src = scan.nextLine();
+        System.out.println("Destination:");
+        String dest = scan.nextLine();
+
+        // TODO:
+        // Read from file src
+        // Write onto dest
+
+
+    }
+}
